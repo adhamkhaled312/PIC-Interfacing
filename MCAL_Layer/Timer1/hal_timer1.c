f1548d9c26e0a53091a51559c58edd128f752cb0
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2023-09-01 23:34:36 +0300

    implement timer 1 driver

---
+++ MCAL_Layer/Timer1/hal_timer1.c
@@ -0,0 +1,156 @@
+/* 
+ * File:   hal_timer1.c
+ * Author: ADHAM KHALED
+ *
+ * Created on September 1, 2023, 12:44 PM
+ */
+#include "hal_timer1.h"
+static inline void timer1_mode_cfg(const timer1_t *timer1);
+static inline void timer1_register_rw_cfg(const timer1_t *timer1);
+static inline void timer1_oscillator_cfg(const timer1_t *timer1);
+static uint16 timer1_preload=0;
+static interrupt_handler timer1_InterruptHandler=NULL;
+void TIMER1_ISR(void){
+    TIMER1_InterruptFlagClear();
+    TMR1H=(timer1_preload)>>8;
+    TMR1L=timer1_preload;
+    if(timer1_InterruptHandler){
+        timer1_InterruptHandler();
+    }
+}
+/**
+ * @brief initialize timer1
+ * @param timer1 refrence to timer1 configurations @ref timer1_t
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_init(const timer1_t *timer1){
+    Std_ReturnType retVal=E_OK;
+    if(NULL==timer1){
+        retVal=E_NOT_OK;
+    }
+    else{
+        TIMER1_DISABLE();
+        T1CONbits.T1CKPS=timer1->prescaler_value;
+        timer1_mode_cfg(timer1);
+        timer1_register_rw_cfg(timer1);
+        timer1_oscillator_cfg(timer1);
+        TMR1H=(timer1->preload_value)>>8;
+        TMR1L=(uint8)(timer1->preload_value);
+        timer1_preload=timer1->preload_value;
+        #if TIMER1_INTERRUPT_ENABLE==INTERRUPT_FEATURE_ENABLE
+        TIMER1_InterruptEnable();
+        TIMER1_InterruptFlagClear();
+        #if INTERRUPT_PRIORITY_LEVELS_ENABLE==INTERRUPT_FEATURE_ENABLE
+        INTERRUPT_PriorityLevelEnable();
+        if(INTERRUPT_HIGH_PRIORITY==timer1->priority){
+            INTERRUPT_GlobalInterruptHighEnable();
+            TIMER1_HighPrioritySet();
+        }
+        else if(INTERRUPT_LOW_PRIORITY==timer1->priority){
+            INTERRUPT_GlobalInterruptLowEnable()
+            TIMER1_LowPrioritySet();
+        }
+        #else
+        INTERRUPT_GlobalInterruptEnable();
+        INTERRUPT_PeripheralInterruptEnable();
+        #endif
+        timer1_InterruptHandler=timer1->timer1_interrupt_handler;
+        #endif
+        TIMER1_ENABLE();
+    }
+    return retVal;
+}
+/**
+ * @brief write a value to timer1 counter
+ * @param timer1 refrence to timer1 configurations @ref timer1_t
+ * @param value the value to be written 
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_deinit(const timer1_t *timer1){
+    Std_ReturnType retVal=E_OK;
+    if(NULL==timer1){
+        retVal=E_NOT_OK;
+    }
+    else{
+        TIMER1_DISABLE();
+        TIMER1_InterruptDisable();
+    }
+    return retVal;
+}
+/**
+ * @brief write a value to timer1 counter
+ * @param timer1 refrence to timer1 configurations @ref timer1_t
+ * @param value the value to be written 
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_write_value(const timer1_t *timer1,uint16 value){
+    Std_ReturnType retVal=E_OK;
+    if(NULL==timer1){
+        retVal=E_NOT_OK;
+    }
+    else{
+        TMR1H=value>>8;
+        TMR1L=value;
+    }
+    return retVal;
+}
+/**
+ * @brief read value from timer1 counter
+ * @param timer1 refrence to timer1 configurations @ref timer1_t
+ * @param value the value read
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_read_value(const timer1_t *timer1,uint16 *value){
+    Std_ReturnType retVal=E_OK;
+    uint8 l_tmr1l=0;
+    uint8 l_tmr1h=0;
+    if(NULL==timer1 || NULL==value){
+        retVal=E_NOT_OK;
+    }
+    else{
+        l_tmr1l=TMR1L;
+        l_tmr1h=TMR1H;
+        *value=(uint16)(l_tmr1l+(l_tmr1h<<8));
+    }
+    return retVal;
+}
+static inline void timer1_mode_cfg(const timer1_t *timer1){
+    if(TIMER1_TIMER_MODE==timer1->mode){
+        TIMER1_TIMER_MODE_ENABLE();
+    }
+    else if(TIMER1_COUNTER_MODE==timer1->mode){
+        TIMER1_COUNTER_MODE_ENABLE();
+        if(TIMER1_ASYNCHRONOUS_MODE==timer1->counter_mode){
+            TIMER1_ASYNCHRONIZE_COUNTER_ENABLE();
+        }
+        else if(TIMER1_SYNCHRONOUS_MODE==timer1->counter_mode){
+            TIMER1_SYNCHRONIZE_COUNTER_ENABLE();
+        }
+    }
+}
+static inline void timer1_register_rw_cfg(const timer1_t *timer1){
+    if(TIMER1_READ_WRITE_8BITS_MODE==timer1->reg_rw_mode){
+        TIMER1_READ_WRITE_8BITS_MODE_ENABLE();
+
+    }
+    else if(TIMER1_READ_WRITE_16BITS_MODE==timer1->reg_rw_mode){
+        TIMER1_READ_WRITE_16BITS_MODE_ENABLE();
+        
+    }
+}
+static inline void timer1_oscillator_cfg(const timer1_t *timer1){
+    if(TIMER1_OSCILLATOR_ENABLE_MODE==timer1->oscillator_mode){
+        TIMER1_OSCILLATOR_ENABLE();
+    }
+    else if(TIMER1_OSCILLATOR_DISABLE_MODE==timer1->oscillator_mode){
+        TIMER1_OSCILLATOR_DISABLE();
+    }
+} 
---
+++ MCAL_Layer/Timer1/hal_timer1.h
@@ -0,0 +1,108 @@
+/* 
+ * File:   hal_timer1.h
+ * Author: ADHAM KHALED
+ *
+ * Created on September 1, 2023, 12:44 PM
+ */
+
+#ifndef HAL_TIMER1_H
+#define	HAL_TIMER1_H
+/* ----------------Includes----------------*/
+#include "../mcal_std_types.h"
+#include "C:\Program Files (x86)\Microchip\MPLABX\v5.35\packs\Microchip\PIC18Fxxxx_DFP\1.2.26\xc8\pic\include\proc\pic18f4620.h"
+#include "../GPIO/hal_gpio.h"
+#include "../Interrupt/mcal_internal_interrupt.h"
+/* ----------------Macro Declarations---------------*/
+
+#define TIMER1_TIMER_MODE               1 
+#define TIMER1_COUNTER_MODE             0 
+
+#define TIMER1_READ_WRITE_8BITS_MODE    1
+#define TIMER1_READ_WRITE_16BITS_MODE   0
+
+#define TIMER1_SYNCHRONOUS_MODE         1
+#define TIMER1_ASYNCHRONOUS_MODE        0
+
+#define TIMER1_OSCILLATOR_ENABLE_MODE   1
+#define TIMER1_OSCILLATOR_DISABLE_MODE  0
+/* ----------------Macro Functions Declarations----------------*/
+#define TIMER1_ENABLE()                        (T1CONbits.TMR1ON=1)
+#define TIMER1_DISABLE()                       (T1CONbits.TMR1ON=0)
+
+#define TIMER1_SYSTEM_CLOCK_STATUS()           (T1CONbits.T1RUN)
+
+#define TIMER1_SYNCHRONIZE_COUNTER_ENABLE()    (T1CONbits.T1SYNC=0)
+#define TIMER1_ASYNCHRONIZE_COUNTER_ENABLE()   (T1CONbits.T1SYNC=1)
+
+#define TIMER1_TIMER_MODE_ENABLE()             (T1CONbits.TMR1CS=0)
+#define TIMER1_COUNTER_MODE_ENABLE()           (T1CONbits.TMR1CS=1)
+
+#define TIMER1_OSCILLATOR_ENABLE()             (T1CONbits.T1OSCEN=1)
+#define TIMER1_OSCILLATOR_DISABLE()          (T1CONbits.T1OSCEN=0)
+
+#define TIMER1_READ_WRITE_8BITS_MODE_ENABLE()  (T1CONbits.RD16=0)
+#define TIMER1_READ_WRITE_16BITS_MODE_ENABLE() (T1CONbits.RD16=1)
+
+/* ----------------Data Type Declarations----------------*/
+/**
+ * @brief prescaler value select 
+ * @note this is used to use less than the frequency of the clock
+ */
+typedef enum{
+    TIMER1_PRESCALER_DIV_1=0,
+    TIMER1_PRESCALER_DIV_2,
+    TIMER1_PRESCALER_DIV_4,
+    TIMER1_PRESCALER_DIV_8,
+}timer1_prescaler_select_t;
+typedef struct{
+    #if TIMER1_INTERRUPT_ENABLE==INTERRUPT_FEATURE_ENABLE
+    void(*timer1_interrupt_handler)(void);
+    interrupt_priority_cfg priority;           /* @ref interrupt_priority_cfg */
+    #endif
+    timer1_prescaler_select_t prescaler_value; /* @ref timer1_prescaler_select_t*/
+    uint16 preload_value;
+    uint8 mode             :1;
+    uint8 counter_mode     :1;
+    uint8 oscillator_mode  :1;
+    uint8 reg_rw_mode      :1;
+
+}timer1_t;
+/* ----------------Functions Declarations----------------*/
+/**
+ * @brief initialize timer1
+ * @param timer0 refrence to timer1 configurations @ref timer1_t
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_init(const timer1_t *timer1);
+/**
+ * @brief write a value to timer1 counter
+ * @param timer0 refrence to timer1 configurations @ref timer1_t
+ * @param value the value to be written 
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_deinit(const timer1_t *timer1);
+/**
+ * @brief write a value to timer1 counter
+ * @param timer0 refrence to timer1 configurations @ref timer1_t
+ * @param value the value to be written 
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_write_value(const timer1_t *timer1,uint16 value);
+/**
+ * @brief read value from timer1 counter
+ * @param timer0 refrence to timer1 configurations @ref timer1_t
+ * @param value the value read
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
+Std_ReturnType timer1_read_value(const timer1_t *timer1,uint16 *value);
+
+#endif	/* HAL_TIMER1_H */
+
