3a0f90bbc5857eee83c3e29bf277288f3e9b31d9
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2023-09-26 23:34:36 +0300

    add documentations

--- MCAL_Layer/USART/hal_usart.h
+++ MCAL_Layer/USART/hal_usart.h
@@ -56,6 +56,7 @@
 /* ----------------Macro Functions Declarations----------------*/
 
 /* ----------------Data Type Declarations----------------*/
+/* baud rate configurations */
 typedef enum{
     BAUDRATE_ASYN_8BIT_lOW_SPEED,
     BAUDRATE_ASYN_8BIT_HIGH_SPEED,
@@ -64,7 +65,7 @@ typedef enum{
     BAUDRATE_SYN_8BIT,
     BAUDRATE_SYN_16BIT
 }baudrate_gen_t;
-
+/* data type for tx configurations */
 typedef struct{
     interrupt_priority_cfg usart_tx_int_priority;
 	uint8 usart_tx_enable : 1;
@@ -72,7 +73,7 @@ typedef struct{
 	uint8 usart_tx_9bit_enable : 1;
     uint8 usart_tx_reserved : 5;
 }usart_tx_cfg_t;
-
+/* data type for rx configurations */
 typedef struct{
     interrupt_priority_cfg usart_rx_int_priority;
 	uint8 usart_rx_enable : 1;
@@ -80,7 +81,6 @@ typedef struct{
 	uint8 usart_rx_9bit_enable : 1;
     uint8 usart_rx_reserved : 5;
 }usart_rx_cfg_t;
-
 typedef union{
 	struct{
 		uint8 usart_tx_reserved : 6;
@@ -89,7 +89,7 @@ typedef union{
 	};
 	uint8 status;
 }usart_error_status_t;
-
+/* data type for usart to be used by the user */
 typedef struct{
     uint32 baudrate;
     baudrate_gen_t baudrate_gen_gonfig;
@@ -102,16 +102,78 @@ typedef struct{
     void (*EUSART_OverrunErrorHandler)(void);
 }usart_t;
 /* ----------------Functions Declarations----------------*/
+/**
+ * @brief initialize usart while running asynchronous
+ * @param _eusart refrence to usart configurations @ref usart_t
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_init(const usart_t *_eusart);
+/**
+ * @brief deinitialize usart while running asynchronous
+ * @param _eusart refrence to usart configurations @ref usart_t
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_deinit(const usart_t *_eusart);
-
+/**
+ * @brief recieve byte throw asynchronous usart but with blocking
+ * @param _data pointer to save the data in
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_ReadByteBlocking(uint8 *_data);
+/**
+ * @brief recieve byte throw asynchronous usart but without blocking
+ * @param _data pointer to save the data in
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_ReadByteNonBlocking(uint8 *_data);
+/**
+ * @brief restart usart in case of overrunning error
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_RX_Restart(void);
-
+/**
+ * @brief send byte throw asynchronous usart but with blocking
+ * @param _data the data to be sent
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_WriteByteBlocking(uint8 _data);
+/**
+ * @brief send string throw asynchronous usart but with blocking
+ * @param _data refrence to the string to be sent
+ * @param str_len length of the string
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_WriteStringBlocking(uint8 *_data, uint16 str_len);
+/**
+ * @brief send byte throw asynchronous usart but without blocking
+ * @param _data the data to be sent
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_WriteByteNonBlocking(uint8 _data);
+/**
+ * @brief send string throw asynchronous usart but without blocking
+ * @param _data refrence to the string to be sent
+ * @param str_len length of the string
+ * @return Std_ReturnType status of the process
+ *         (E_OK): The function is done successfully
+ *         (E_NOK): The function had issue to perform this action  
+ */
 Std_ReturnType eusart_async_WriteStringNonBlocking(uint8 *_data, uint16 str_len);
 #endif	/* HAL_USART_H */
 
